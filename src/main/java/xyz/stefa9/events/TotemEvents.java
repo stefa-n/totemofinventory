094eae59ea85cbb2983307d44bcc28be09e477d8
stefa-n <67799417+stefa-n@users.noreply.github.com>
2024-04-11 21:17:31 +0300

    Add: Auto respawn & play totem effect

--- src/main/java/xyz/stefa9/events/TotemEvents.java
+++ src/main/java/xyz/stefa9/events/TotemEvents.java
@@ -1,11 +1,14 @@
 package xyz.stefa9.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.EntityEffect;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.Inventory;
 import xyz.stefa9.items.ItemManager;
+import xyz.stefa9.totemofinventory;
 
 public class TotemEvents implements Listener {
     @EventHandler
@@ -22,6 +25,14 @@ public class TotemEvents implements Listener {
 
             inventory.removeItem(ItemManager.TotemOfInventory);
             inventory.addItem(ItemManager.BrokenTotemOfInventory);
+
+            Bukkit.getScheduler().scheduleSyncDelayedTask(totemofinventory.getPlugin(totemofinventory.class), new Runnable() {
+                @Override
+                public void run() {
+                    player.spigot().respawn();
+                    player.playEffect(EntityEffect.TOTEM_RESURRECT);
+                }
+            }, 1L);
         }
     }
 }
